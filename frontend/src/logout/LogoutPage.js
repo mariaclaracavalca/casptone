9eb4e6dcd676663d76f36d803c02f84a4577edbc
Maria Cavalca <maria.cavalca@xfarm.ag>
2024-10-03 12:59:37 +0200

    class logout

--- frontend/src/logout/LogoutPage.js
+++ frontend/src/logout/LogoutPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './LogoutPage.css';
 
 const LogoutPage = () => {
   const navigate = useNavigate();
@@ -11,10 +12,10 @@ const LogoutPage = () => {
   };
 
   return (
-    <div className="auth-container">
-      <div className="auth-box">
+    <div className="logout-container">
+      <div className="logout-box">
         <h1>Sei sicuro di voler disconnetterti?</h1>
-        <button className="auth-button" onClick={handleLogout}>Logout</button>
+        <button className="logout-button" onClick={handleLogout}>Logout</button>
       </div>
     </div>
   );
